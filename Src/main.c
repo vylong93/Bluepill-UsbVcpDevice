ca12160f3d8b2328b8e274816d7c3f1e26787c53
Long Vy. Dang <vylong93khtn@gmail.com>
2021-01-01 18:52:53 +0700

    Generate:TIM4 CH4 trigger ADC1
    Signed-off-by: Long Vy. Dang <vylong93khtn@gmail.com>

--- Src/main.c
+++ Src/main.c
@@ -193,9 +193,9 @@ static void MX_ADC1_Init(void)
   */
   hadc1.Instance = ADC1;
   hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
-  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
-  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T4_CC4;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
   hadc1.Init.NbrOfConversion = 1;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
